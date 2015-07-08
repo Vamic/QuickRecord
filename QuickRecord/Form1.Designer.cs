1b412530776f738e4e94ece2a04778fc38442e54
Vamic <made.swede@live.com>
2015-07-08 19:19:50 +0200

    all of everything

---
+++ QuickRecord/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace QuickRecord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnChangeKeys = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.hotkeyTextBox = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderLocation = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.showNotifications = new System.Windows.Forms.CheckBox();
+            this.openFolderButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChangeKeys
+            // 
+            this.btnChangeKeys.Location = new System.Drawing.Point(179, 12);
+            this.btnChangeKeys.Name = "btnChangeKeys";
+            this.btnChangeKeys.Size = new System.Drawing.Size(93, 22);
+            this.btnChangeKeys.TabIndex = 0;
+            this.btnChangeKeys.Text = "Change";
+            this.btnChangeKeys.UseVisualStyleBackColor = true;
+            this.btnChangeKeys.Click += new System.EventHandler(this.btnChangeKeys_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "QuickRecord v1.1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuSettings,
+            this.menuExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 48);
+            // 
+            // menuSettings
+            // 
+            this.menuSettings.Name = "menuSettings";
+            this.menuSettings.Size = new System.Drawing.Size(116, 22);
+            this.menuSettings.Text = "Settings";
+            this.menuSettings.Click += new System.EventHandler(this.menuSettings_Click);
+            // 
+            // menuExit
+            // 
+            this.menuExit.Name = "menuExit";
+            this.menuExit.Size = new System.Drawing.Size(116, 22);
+            this.menuExit.Text = "Exit";
+            this.menuExit.Click += new System.EventHandler(this.menuExit_Click);
+            // 
+            // hotkeyTextBox
+            // 
+            this.hotkeyTextBox.Location = new System.Drawing.Point(12, 14);
+            this.hotkeyTextBox.Name = "hotkeyTextBox";
+            this.hotkeyTextBox.ReadOnly = true;
+            this.hotkeyTextBox.Size = new System.Drawing.Size(161, 20);
+            this.hotkeyTextBox.TabIndex = 1;
+            this.hotkeyTextBox.Text = "Ctrl+Shift+6";
+            this.hotkeyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hotkeyTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.hotkeyTextBox_KeyDown);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 40);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(223, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Until the key combination is pressed again";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(12, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Specified time";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(109, 63);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // folderLocation
+            // 
+            this.folderLocation.Location = new System.Drawing.Point(11, 109);
+            this.folderLocation.Name = "folderLocation";
+            this.folderLocation.ReadOnly = true;
+            this.folderLocation.Size = new System.Drawing.Size(153, 20);
+            this.folderLocation.TabIndex = 5;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(170, 107);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(30, 22);
+            this.browseButton.TabIndex = 6;
+            this.browseButton.Text = "...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(11, 86);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Save recordings to a folder";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Max record length";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Enabled = false;
+            this.numericUpDown2.Location = new System.Drawing.Point(110, 130);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            // 
+            // showNotifications
+            // 
+            this.showNotifications.AutoSize = true;
+            this.showNotifications.Checked = true;
+            this.showNotifications.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.showNotifications.Location = new System.Drawing.Point(12, 153);
+            this.showNotifications.Name = "showNotifications";
+            this.showNotifications.Size = new System.Drawing.Size(112, 17);
+            this.showNotifications.TabIndex = 10;
+            this.showNotifications.Text = "Show notifications";
+            this.showNotifications.UseVisualStyleBackColor = true;
+            // 
+            // openFolderButton
+            // 
+            this.openFolderButton.Location = new System.Drawing.Point(221, 107);
+            this.openFolderButton.Name = "openFolderButton";
+            this.openFolderButton.Size = new System.Drawing.Size(51, 22);
+            this.openFolderButton.TabIndex = 11;
+            this.openFolderButton.Text = "Open";
+            this.openFolderButton.UseVisualStyleBackColor = true;
+            this.openFolderButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 177);
+            this.Controls.Add(this.openFolderButton);
+            this.Controls.Add(this.showNotifications);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.folderLocation);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.hotkeyTextBox);
+            this.Controls.Add(this.btnChangeKeys);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeKeys;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuSettings;
+        private System.Windows.Forms.ToolStripMenuItem menuExit;
+        private System.Windows.Forms.TextBox hotkeyTextBox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox folderLocation;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.CheckBox showNotifications;
+        private System.Windows.Forms.Button openFolderButton;
+    }
+}
+
---
+++ QuickRecord/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using Hotkeys;
+
+namespace QuickRecord
+{
+
+    public partial class Form1 : Form
+    {
+        NAudio.Wave.WasapiLoopbackCapture sourceStream;
+        NAudio.Wave.WaveFileWriter waveWriter;
+        Hotkeys.GlobalHotkey ghk;
+        bool recording;
+        string pathend;
+        Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ghk = new GlobalHotkey(6, Keys.D6, this);
+            ghk.Register();
+            timer = new Timer();
+            timer.Interval = 120000; //max recording of 120000ms (2 minutes) just in case, should be changable maybe
+            timer.Tick += new EventHandler(timer_Tick);
+
+
+            pathend = "QuickRecording" + ".wav";
+
+
+            notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon.BalloonTipTitle = "QuickRecord";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            folderLocation.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+
+        private void HandleHotkey()
+        {
+            if (!recording)
+            {
+                sourceStream = new NAudio.Wave.WasapiLoopbackCapture();
+                sourceStream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourceStream_DataAvailable);
+                string pathstart = folderLocation.Text + "\\" + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+                waveWriter = new NAudio.Wave.WaveFileWriter(pathstart+pathend, sourceStream.WaveFormat);
+
+                sourceStream.StartRecording();
+                recording = true;
+                timer.Start();
+
+                notifyIcon.BalloonTipText = "Started recording.";
+                if (showNotifications.Checked)
+                    notifyIcon.ShowBalloonTip(1000);
+
+            }
+            else
+            {
+                StopRecording();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (recording)
+            {
+                StopRecording();
+            }
+        }
+
+        private void StopRecording()
+        {
+            sourceStream.StopRecording();
+            sourceStream.Dispose();
+            waveWriter.Dispose();
+            recording = false;
+            timer.Stop();
+            notifyIcon.BalloonTipText = "Recording finished.";
+            if (showNotifications.Checked)
+                notifyIcon.ShowBalloonTip(1000);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+            {
+                HandleHotkey();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void sourceStream_DataAvailable(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            if (waveWriter == null)
+                return;
+
+            waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+
+        private void btnChangeKeys_Click(object sender, EventArgs e)
+        {
+            if (btnChangeKeys.Text == "Change")
+            {
+                hotkeyTextBox.Text = "";
+                hotkeyTextBox.SelectAll();
+                btnChangeKeys.Text = "Save";
+            }
+            else
+            {
+                if (hotkeyTextBox.Text == "")
+                {
+                    hotkeyTextBox.Text = "Ctrl+Shift+6";
+                }
+                btnChangeKeys.Text = "Change";
+            }
+        }
+
+        private void hotkeyTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (btnChangeKeys.Text != "Change")
+            {
+                Keys modifierKeys = e.Modifiers;
+
+                Keys pressedKey = e.KeyData ^ modifierKeys ^ Keys.ShiftKey;
+
+                if (modifierKeys != Keys.None && pressedKey != Keys.None)
+                {
+                    if (pressedKey != Keys.LButton && pressedKey != Keys.RButton)
+                    {
+                        var converter = new KeysConverter();
+                        hotkeyTextBox.Text = converter.ConvertToString(e.KeyData);
+                    }
+                }
+            }
+        }
+
+        #region Boring Shit
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void menuSettings_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void menuExit_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+        }
+
+        private void Shutdown()
+        {
+            ghk.Unregister();
+
+            if (sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+            }
+            if (waveWriter != null)
+            {
+                waveWriter.Dispose();
+            }
+
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                folderLocation.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //browseButton.Enabled = checkBox1.Checked;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", folderLocation.Text);
+        }
+        #endregion
+    }
+}
